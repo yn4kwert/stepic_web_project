a8f88ba294fbcdb9c19963b7ab7a318953d3fad5
Vadim Maslov <wado93@mail.ru>
2022-07-24 18:24:34 +0700

    added wsgi and modified conf

---
+++ hello.py
@@ -0,0 +1,10 @@
+def wsgi_app(environ, start_response):
+    #
+    status = '200 OK'
+    headers = [
+        ('Content-type', 'text/plain')
+    ]
+    body = [(i + '\n') for i in environ['QUERY_STRING'].split('&')]
+    #body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+    start_response(status, headers)
+    return body
