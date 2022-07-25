d661810481899aba74459ceb9ee2be93fa3eb837
Vadim Maslov <wado93@mail.ru>
2022-07-25 22:47:36 +0700

    10

--- init.sh
+++ init.sh
@@ -1,6 +1,4 @@
-sudo unlink /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo rm -rf /etc/nginx/sites-enabled/default
+sudo cp /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/hello
-sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/django
-sudo /etc/init.d/gunicorn restart
+sudo gunicorn -b 0.0.0.0:8080 hello:wsgi_app
