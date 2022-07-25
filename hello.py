e2771436b6afb0a3abe8124d897ab40da7545ecb
Vadim Maslov <wado93@mail.ru>
2022-07-25 23:16:57 +0700

    12

--- hello.py
+++ hello.py
@@ -1,8 +1,12 @@
 def wsgi_app(environ, start_response):
+    print(environ)
     status = '200 OK'
     headers = [
         ('Content-Type', 'text/plain')
     ]
+    #body = [(i + '\n') for i in environ['QUERY_STRING'].split('&')]
     body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+    print(headers)
+    print('\n\n', body)
     start_response(status, headers)
     return body
