0c81e3990633f6bd15a040b0551a4d61851b7e3c
Vadim Maslov <wado93@mail.ru>
2022-07-25 22:58:17 +0700

    11

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo rm -rf /etc/nginx/sites-enabled/default
-sudo cp /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled
+sudo cp /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled
 sudo /etc/init.d/nginx restart
 sudo gunicorn -b 0.0.0.0:8080 hello:wsgi_app
